6fafdc2b5182d82e5824207e73da3fb0f1133277
Peter Wolf <PeterWolf@online.de>
2022-04-26 21:24:26 +0200

    Hauptmenü

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { NewGameComponent } from './new-game/new-game.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: MenuComponent},
+    { path: 'newgame', component: NewGameComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
